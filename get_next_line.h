0ab4b6b2548993ff66ab21e3f0251c36c68b9a43
dbessa <danielbessa01@gmail.com>
2023-10-25 18:25:23 -0300

    new: add functions in header

--- get_next_line.h
+++ get_next_line.h
@@ -6,8 +6,18 @@
 /*   By: dbessa <dbessa@student.42.rio>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/22 20:19:01 by dbessa            #+#    #+#             */
-/*   Updated: 2023/10/22 20:19:04 by dbessa           ###   ########.fr       */
+/*   Updated: 2023/10/24 13:49:00 by dbessa           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
 
+# include <unistd.h>
+
+char	*get_next_line(int fd);
+ft_strdup
+ft_strjoin
+read(int fd, void *buf, size_t nbyte)
+
+#endif
