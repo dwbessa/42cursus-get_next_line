66ed9166669c7b234b3e5f78fe562b539a7248ba
dbessa <danielbessa01@gmail.com>
2023-11-03 16:25:11 -0300

    feat: initiating the function

--- get_next_line.c
+++ get_next_line.c
@@ -6,10 +6,17 @@
 /*   By: dbessa <dbessa@student.42.rio>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/22 20:17:54 by dbessa            #+#    #+#             */
-/*   Updated: 2023/10/22 20:20:42 by dbessa           ###   ########.fr       */
+/*   Updated: 2023/11/03 16:24:38 by dbessa           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 char	*get_next_line(int fd)
 {
+    char *line_read;
+    
+    if (fd < 0)
+        return (NULL);
+    
+
+    return (line_read)
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,14 +6,22 @@
 /*   By: dbessa <dbessa@student.42.rio>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/22 20:19:01 by dbessa            #+#    #+#             */
-/*   Updated: 2023/10/24 13:49:00 by dbessa           ###   ########.fr       */
+/*   Updated: 2023/11/03 10:11:31 by dbessa           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
+# include <stdlib.h>
 # include <unistd.h>
+
+typedef struct s_list
+{
+	void			*content;
+	struct s_list	*next;
+}				t_list;
+
 
 char	*get_next_line(int fd);
 ft_strdup
